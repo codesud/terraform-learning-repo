6c4ffdc5b2e3b9adb9210e16783d650c41b6fa50
Sudeep S <sudeepdirect@gmail.com>
2022-10-18 12:29:16 +0530

    Added tf files

--- 03-tf-remote-state/state.tf
+++ 03-tf-remote-state/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "b49-rf-remote-state-bucket"
+    bucket = "b49-tf-remote-state-bucket"
     key    = "sample/terraform.tfstate"
     region = "us-east-1"
   }
