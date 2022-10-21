8e2cda378ba5fac2595252b33fa8a1cb40bfeb4c
Sudeep S <sudeepdirect@gmail.com>
2022-10-21 13:34:34 +0530

    Updated tf files

--- robot-basic-setup/vars.tf
+++ robot-basic-setup/vars.tf
@@ -4,30 +4,30 @@ variable "ALL_COMPONENTS" {
         app_version = "null"
        },
        catalogue = {
-        app_version = ""
+        app_version = "3.0.0"
        },
        redis = {
         app_version = "null"
        },
        user = {
-        app_version = "0.0.1"
+        app_version = "3.0.0"
        },
        cart = {
-        app_version = "0.0.2"
+        app_version = "3.0.0"
        },
        mysql = {
         app_version = "null"
        },
        shipping = {
-        app_version = "0.0.1"
+        app_version = "3.0.0"
        },
        rabbitmq = {
         app_version = "null"
        },
        payment = {
-        app_version = "0.0.1"
+        app_version = "3.0.0"
        },       frontend = {
-        app_version = "0.0.2"
+        app_version = "3.0.0"
        }
     }
 }
