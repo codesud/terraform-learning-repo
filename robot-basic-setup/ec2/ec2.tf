49fbeae1435f2f5e9be7fe6005d6d4c00dfa985f
Sudeep S <sudeepdirect@gmail.com>
2022-10-21 17:03:08 +0530

    Updated tf files

--- robot-basic-setup/ec2/ec2.tf
+++ robot-basic-setup/ec2/ec2.tf
@@ -7,18 +7,21 @@ resource "aws_spot_instance_request" "cheap_worker" {
   tags = {
     Name = var.COMPONENT
   }
-
-  connection {
+}
+resource "null_resource" "null" {
+  triggers = {    
+        a = timestamp()  # Everytime you run, when compared to the last time, the time changes, so it will be triggered all the time.
+  }
+  provisioner "remote-exec" {
+      connection {
         type     = "ssh"
         user     = "centos"
         password = "DevOps321"
         # host     = self.public_ip
         host     = aws_spot_instance_request.cheap_worker.private_ip 
       } 
-  provisioner "remote-exec" {
     inline = [
-     "ansible-pull -U https://github.com/CodingSudeep/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e TAG_NAME=${var.APP_VERSION} roboshop.yml"
+     "ansible-pull -U https://github.com/b49-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e TAG_NAME=${var.APP_VERSION} roboshop.yml"
       ]
     }
-} 
-
+}
