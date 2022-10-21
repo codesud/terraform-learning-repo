03c9160c23a4aeec68ca8e62b08193e87b84d758
Sudeep S <sudeepdirect@gmail.com>
2022-10-21 13:02:58 +0530

    Updated tf files

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -3,22 +3,19 @@ resource "aws_instance" "b49-ec2" {
   ami                     = "ami-063dd2bd5641ef903"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = [var.sg]
-}
-
-provisioner "remote-exec" {
-
-      connection {
-        type     = "ssh"
-        user     = "centos"
-        password = "DevOps321"
-        host     = self.public_ip
-      }
-
+  
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
     inline = [
-     "ansible-pull -U https://github.com/CodingSudeep/ansible.git -e COMPONENT=frontend -e ENV=dev -e TAG_NAME=3.0.0 roboshop.yml"
+    "ansible-pull -U https://github.com/CodingSudeep/ansible.git -e COMPONENT=frontend -e ENV=dev -e TAG_NAME=3.0.0 roboshop.yml"
     ]
+  }
 }
-
 
 variable "sg" {}
 
