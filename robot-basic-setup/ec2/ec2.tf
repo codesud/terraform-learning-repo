0d133ea35521810b7d391178cba07b6879f1739f
Sudeep S <sudeepdirect@gmail.com>
2022-10-22 09:30:43 +0530

    Updated tf files

--- robot-basic-setup/ec2/ec2.tf
+++ robot-basic-setup/ec2/ec2.tf
@@ -3,12 +3,9 @@ resource "aws_spot_instance_request" "cheap_worker" {
   instance_type          = "t3.micro"
   wait_for_fulfillment   = true 
   vpc_security_group_ids = [aws_security_group.allow_all.id]
-
   tags = {
     Name = var.COMPONENT
   }
-}
-provisioner "remote-exec" { 
   connection {
     type     = "ssh"
     user     = "centos"
@@ -16,9 +13,9 @@ provisioner "remote-exec" {
     # host     = self.public_ip
     host     = aws_spot_instance_request.cheap_worker.private_ip 
   } 
-   
-  inline = [
-  "ansible-pull -U https://github.com/CodingSudeep/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e TAG_NAME=${var.APP_VERSION} roboshop.yml"
-  ]
+  provisioner "remote-exec" {  
+    inline = [
+    "ansible-pull -U https://github.com/CodingSudeep/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e TAG_NAME=${var.APP_VERSION} roboshop.yml"
+    ]
+  }
 }
-
