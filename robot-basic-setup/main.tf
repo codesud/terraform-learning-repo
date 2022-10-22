fd25f371eab13a87fe262d036b5aa4c20c5ae84f
Sudeep S <sudeepdirect@gmail.com>
2022-10-22 09:05:45 +0530

    Updated tf files

--- robot-basic-setup/main.tf
+++ robot-basic-setup/main.tf
@@ -1,12 +1,10 @@
 module "frontend" {
-
     source       = "./ec2"
     COMPONENT    = "frontend"
     APP_VERSION  = "3.0.0"
 }
 
 module "cart" {
-
     source       = "./ec2"
     COMPONENT    = "cart"
     APP_VERSION  = "3.0.0"
